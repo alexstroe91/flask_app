225fad6f68fc67c49748c527c36192193811386a
marcos hernando lopez <ratija1984@hotmail.com>
2022-04-06 10:21:24 +0100

    Reorganización y nuevas funciones

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-
-from flask import Flask, render_template, request
+from flask import Flask, flash, redirect, render_template, request, url_for
+from flask_login import LoginManager, current_user, login_user, logout_user
 from models import *
 from config import *
 from werkzeug.security import check_password_hash
@@ -7,56 +7,93 @@ from werkzeug.security import check_password_hash
 
 app = Flask(__name__)
 setup(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
+
 db.init_app(app)
 
 @app.route('/')
 def index():
+    #ELIMINAR O EDITAR A POSTERIORI, AHORA CON ACCESO A PÁGINAS PARA TESTEO
     return render_template('index.html')
-
 
 @app.route('/login', methods = ['POST', 'GET'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+
+    #Inicia con el método GET
     email = request.form.get('email')
     password = request.form.get('password')
     user = UserModel.query.filter_by(email = email).first()
+
+    #Al rellenar el formulario y presionar el botón pasa por la verficación
     if request.method == 'POST':
         if user and check_password_hash(user.password, password):
+            login_user(user, remember = request.form.get('remember'))
             return render_template('index.html')
-        else:
-            return render_template('login.html', msg = "Revise sus credenciales.") 
-    
+        elif not user:
+            flash("Usuario no encontrado")
+        elif not check_password_hash(user.password, password):
+            flash("Contraseña incorrecta")
+        return render_template('login.html') 
+    #Carga de HTML del método GET
     return render_template('login.html')
-
 
 @app.route('/signup', methods = ['GET', 'POST'])
 def signup():
+    created = False
     if request.method == 'POST':
         username = request.form.get('username')
         email = request.form.get('email')
         password = request.form.get('password')
         confirmpassword = request.form.get('confirmpassword')
-
+        
+        #Verifica si el email está en uso o no
         emailRegistered = UserModel.query.filter_by(email = email).first()
+        #Verifica si el nombre está en uso o no
         nameInUse = UserModel.query.filter_by(name = username).first()
 
+        #Validación del registro
         if password != confirmpassword:
-            return render_template('signup.html', msg = "Las contraseñas no coinciden")
+            flash("Las contraseñas no coinciden")
         elif emailRegistered:
-            return render_template('signup.html', msg = "Email ya registrado")
+            flash("Email ya registrado")
         elif nameInUse:
-            return render_template('signup.html', msg = "Nombre en uso")
-            
+            flash("Nombre ya en uso")
+        #Si está todo bien crea el usuario
         else:
             new_user = UserModel(name = username, email = email, password = password)
             db.session.add(new_user)
             db.session.commit()
-            return render_template('signup.html', msg = "Usuario creado")
+            created = True
+            flash("Usuario registrado exitosamente")
 
-    return render_template('signup.html')
+        return render_template('signup.html', created = created)
+    #Carga de HTML del método GET
+    return render_template('signup.html', created = created)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
 
 
 
 if __name__ == "__main__":
     app.run(debug = True)
 
-    
+
+
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    user = UserModel.query.filter_by(id = user_id).first()
+    if user:
+        return user
+    return None  
